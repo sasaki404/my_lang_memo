6e4fdce046da97719ae000afc19e254b53331e7c
sasaki404 <takaaki.sasaki2001@gmail.com>
2023-11-27 20:05:44 +0900

    update WordRecordsNotifier to update, delete

--- lib/provider/word_records.dart
+++ lib/provider/word_records.dart
@@ -12,11 +12,24 @@ class WordRecordsNotifier extends _$WordRecordsNotifier {
 
   Future<int> insert(Word word) async {
     int id = await WordTable().insert(word);
+    // TODO: 全件取得する必要はない
     state = AsyncData(await WordTable().selectAll());
     return id;
   }
 
+  Future<void> updateState(Word info) async {
+    await WordTable().update(info);
+    // TODO: 全件取得する必要はない
+    state = AsyncData(await WordTable().selectAll());
+  }
+
   Future<List<Word>> selectAll() async {
     return await WordTable().selectAll();
+  }
+
+  Future<void> delete(int id) async {
+    await WordTable().delete(id);
+    // TODO: 全件取得する必要はない
+    state = AsyncData(await WordTable().selectAll());
   }
 }
